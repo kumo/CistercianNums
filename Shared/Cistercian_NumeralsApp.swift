736b74861467f031da7be80b1d154daf534f9ce1
Robert Clarke <kumo@kumo.it>
2021-02-03 20:46:31 +0100

    Initial Commit

---
+++ Shared/Cistercian_NumeralsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cistercian_NumeralsApp.swift
+//  Shared
+//
+//  Created by Robert Clarke on 03/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Cistercian_NumeralsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
