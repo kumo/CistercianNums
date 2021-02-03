d637930cb5a473658710af07fbad7028af7316e2
Robert Clarke <kumo@kumo.it>
2021-02-03 21:33:46 +0100

    Showing a number from 1 to 9

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,123 @@
 import SwiftUI
 
 struct ContentView: View {
+    var number = 9
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geometry in
+            
+            Path { path in
+                let width: CGFloat = min(geometry.size.width, geometry.size.height)
+                let height = width
+                
+                path.move(
+                    to: CGPoint(
+                        x: width * 0.5,
+                        y: height * 0.0
+                    )
+                )
+                path.addLine(to: CGPoint(x: width * 0.5, y: height * 1.0))
+                
+                if number == 1 {
+                    path.move(
+                        to: CGPoint(
+                            x: width * 0.5,
+                            y: height * 0.0
+                        )
+                    )
+                    path.addLine(to: CGPoint(x: width * 0.6, y: height * 0.0))
+                }
+                
+                if number == 2 {
+                    path.move(
+                        to: CGPoint(
+                            x: width * 0.5,
+                            y: height * 0.1
+                        )
+                    )
+                    path.addLine(to: CGPoint(x: width * 0.6, y: height * 0.1))
+                }
+                
+                if number == 3 {
+                    path.move(
+                        to: CGPoint(
+                            x: width * 0.5,
+                            y: height * 0.0
+                        )
+                    )
+                    path.addLine(to: CGPoint(x: width * 0.6, y: height * 0.1))
+                }
+                
+                if number == 4 {
+                    path.move(
+                        to: CGPoint(
+                            x: width * 0.5,
+                            y: height * 0.1
+                        )
+                    )
+                    path.addLine(to: CGPoint(x: width * 0.6, y: height * 0.0))
+                }
+                
+                if number == 5 {
+                    path.move(
+                        to: CGPoint(
+                            x: width * 0.5,
+                            y: height * 0.25
+                        )
+                    )
+                    path.addLine(to: CGPoint(x: width * 0.75, y: height * 0.0))
+                    path.addLine(to: CGPoint(x: width * 0.5, y: height * 0.0))
+                }
+                
+                if number == 6 {
+                    path.move(
+                        to: CGPoint(
+                            x: width * 0.75,
+                            y: height * 0.25
+                        )
+                    )
+                    path.addLine(to: CGPoint(x: width * 0.75, y: height * 0.0))
+                }
+                
+                if number == 7 {
+                    path.move(
+                        to: CGPoint(
+                            x: width * 0.75,
+                            y: height * 0.25
+                        )
+                    )
+                    path.addLine(to: CGPoint(x: width * 0.75, y: height * 0.0))
+                    path.addLine(to: CGPoint(x: width * 0.5, y: height * 0.0))
+                }
+                
+                if number == 8 {
+                    path.move(
+                        to: CGPoint(
+                            x: width * 0.75,
+                            y: height * 0.0
+                        )
+                    )
+                    path.addLine(to: CGPoint(x: width * 0.75, y: height * 0.25))
+                    path.addLine(to: CGPoint(x: width * 0.5, y: height * 0.25))
+                }
+                
+                if number == 9 {
+                    path.move(
+                        to: CGPoint(
+                            x: width * 0.5,
+                            y: height * 0.0
+                        )
+                    )
+                    path.addLine(to: CGPoint(x: width * 0.75, y: height * 0.0))
+                    path.addLine(to: CGPoint(x: width * 0.75, y: height * 0.25))
+                    path.addLine(to: CGPoint(x: width * 0.5, y: height * 0.25))
+                }
+                
+            }
+            .stroke(Color.green, style: StrokeStyle(lineWidth: 4.0, lineCap: .round))
+        }
+        .aspectRatio(1, contentMode: .fit)
+        
     }
 }
 
