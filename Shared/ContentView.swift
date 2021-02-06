fd2c9b5ca06097a1db2538da66a2ec341d359e90
Robert Clarke <kumo@kumo.it>
2021-02-06 20:43:55 +0100

    Adding an increment button and moving Cistercian Numeral drawing to a different view

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -32,102 +32,9 @@ struct CistercianDrawing: OptionSet {
     static let bottomLeftV = CistercianDrawing(rawValue: 1 << 19)
 }
 
-func convertValue(value: Int) -> CistercianDrawing {
-    let data: [(Int, CistercianDrawing)] = [
-        (9000, [.bottomLeftV, .bottomMiddleLeftH, .bottomLeftH]),
-        (8000, [.bottomLeftV, .bottomMiddleLeftH]),
-        (7000, [.bottomLeftV, .bottomLeftH]),
-        (6000, [.bottomLeftV]),
-        (5000, [.bottomLeftDiagUp, .bottomLeftH]),
-        (4000, [.bottomLeftDiagUp]),
-        (3000, [.bottomMiddleLeftDiagDown]),
-        (2000, [.bottomMiddleLeftH]),
-        (1000, [.bottomLeftH]),
-        (900, [.bottomRightV, .bottomMiddleRightH, .bottomRightH]),
-        (800, [.bottomRightV, .bottomMiddleRightH]),
-        (700, [.bottomRightV, .bottomRightH]),
-        (600, [.bottomRightV]),
-        (500, [.bottomRightDiagUp, .bottomRightH]),
-        (400, [.bottomRightDiagUp]),
-        (300, [.bottomRightDiagDown]),
-        (200, [.bottomMiddleRightH]),
-        (100, [.bottomRightH]),
-        (90, [.topLeftV, .topMiddleLeftH, .topLeftH]),
-        (80, [.topLeftV, .topMiddleLeftH]),
-        (70, [.topLeftV, .topLeftH]),
-        (60, [.topLeftV]),
-        (50, [.topLeftDiagUp, .topLeftH]),
-        (40, [.topLeftDiagUp]),
-        (30, [.topLeftDiagDown]),
-        (20, [.topMiddleLeftH]),
-        (10, [.topLeftH]),
-        (9, [.topRightV, .topMiddleRightH, .topRightH]),
-        (8, [.topRightV, .topMiddleRightH]),
-        (7, [.topRightV, .topRightH]),
-        (6, [.topRightV]),
-        (5, [.topRightDiagUp, .topRightH]),
-        (4, [.topRightDiagUp]),
-        (3, [.topRightDiagDown]),
-        (2, [.topMiddleRightH]),
-        (1, [.topRightH])
-    ]
+struct CistercianNumeralView: View {
     
-    var convertableValue = value
-    
-    var result = CistercianDrawing()
-    
-    for (number, lines) in data {
-        if number <= convertableValue {
-            result.insert(lines)
-            convertableValue = convertableValue - number
-        }
-    }
-    
-    return result
-}
-
-func convertValue2(value: Int) -> CistercianDrawing {
-    var conversionData: [(Int, CistercianDrawing)] = [
-        (9000, [.bottomLeftV, .bottomMiddleLeftH, .bottomLeftH]),
-        (8000, [.bottomLeftV, .bottomMiddleLeftH]),
-        (7000, [.bottomLeftV, .bottomLeftH]),
-        (6000, [.bottomLeftV]),
-        (5000, [.bottomMiddleLeftDiagDown, .bottomLeftH]),
-        (4000, [.bottomMiddleLeftDiagDown]),
-        (3000, [.bottomLeftDiagUp]),
-        (2000, [.bottomMiddleLeftH]),
-        (1000, [.bottomLeftH]),
-    ]
-
-    var convertMe = value
-    var result = CistercianDrawing()
-
-    for _ in 0...3 {
-        for (index, data) in conversionData.enumerated() {
-            if data.0 <= convertMe {
-                result.insert(data.1)
-                convertMe = convertMe - data.0
-            }
-            
-            conversionData[index] = (data.0 / 10, CistercianDrawing(rawValue: data.1.rawValue >> 5))
-        }
-    }
-    
-    return result
-}
-
-struct ContentView: View {
-//    var drawing: CistercianDrawing = [
-//        .topRightDiagDown, .topLeftH, .topLeftV,
-//        .topMiddleLeftH, .bottomLeftH, .bottomRightH,
-//        .bottomRightV, .bottomMiddleRightH] // 1993
-    
-//    var drawing: CistercianDrawing = [
-//        .topRightDiagDown, .topLeftDiagDown,
-//        .bottomRightDiagDown,
-//        .bottomLeftV, .bottomLeftH, .bottomMiddleLeftH] // 9433
-    
-    var drawing = convertValue(value: 9433)
+    var drawing: CistercianDrawing
     
     var body: some View {
         GeometryReader { geometry in
@@ -303,6 +210,41 @@ struct ContentView: View {
         }
         .aspectRatio(1, contentMode: .fit)
         
+    }}
+
+struct ContentView: View {
+    //    var drawing: CistercianDrawing = [
+    //        .topRightDiagDown, .topLeftH, .topLeftV,
+    //        .topMiddleLeftH, .bottomLeftH, .bottomRightH,
+    //        .bottomRightV, .bottomMiddleRightH] // 1993
+    
+    //    var drawing: CistercianDrawing = [
+    //        .topRightDiagDown, .topLeftDiagDown,
+    //        .bottomRightDiagDown,
+    //        .bottomLeftV, .bottomLeftH, .bottomMiddleLeftH] // 9433
+    
+    @State var drawing = convertValue(value: 1)
+    @State var input = 0
+    
+    var body: some View {
+        VStack {
+            CistercianNumeralView(drawing: drawing)
+            
+            Button(action: {
+                input = input + 1
+                self.drawing = convertValue2(value: input)
+            }) {
+                HStack {
+                    Image(systemName: "plus")
+                    Text("Increment")
+                }
+                .padding(10.0)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10.0)
+                    .stroke(lineWidth: 2.0)
+                )
+            }
+        }
     }
 }
 
---
+++ Shared/Converters.swift
@@ -0,0 +1,93 @@
+//
+//  Converters.swift
+//  Cistercian Numerals
+//
+//  Created by Robert Clarke on 06/02/21.
+//
+
+import Foundation
+
+
+func convertValue(value: Int) -> CistercianDrawing {
+    let data: [(Int, CistercianDrawing)] = [
+        (9000, [.bottomLeftV, .bottomMiddleLeftH, .bottomLeftH]),
+        (8000, [.bottomLeftV, .bottomMiddleLeftH]),
+        (7000, [.bottomLeftV, .bottomLeftH]),
+        (6000, [.bottomLeftV]),
+        (5000, [.bottomLeftDiagUp, .bottomLeftH]),
+        (4000, [.bottomLeftDiagUp]),
+        (3000, [.bottomMiddleLeftDiagDown]),
+        (2000, [.bottomMiddleLeftH]),
+        (1000, [.bottomLeftH]),
+        (900, [.bottomRightV, .bottomMiddleRightH, .bottomRightH]),
+        (800, [.bottomRightV, .bottomMiddleRightH]),
+        (700, [.bottomRightV, .bottomRightH]),
+        (600, [.bottomRightV]),
+        (500, [.bottomRightDiagUp, .bottomRightH]),
+        (400, [.bottomRightDiagUp]),
+        (300, [.bottomRightDiagDown]),
+        (200, [.bottomMiddleRightH]),
+        (100, [.bottomRightH]),
+        (90, [.topLeftV, .topMiddleLeftH, .topLeftH]),
+        (80, [.topLeftV, .topMiddleLeftH]),
+        (70, [.topLeftV, .topLeftH]),
+        (60, [.topLeftV]),
+        (50, [.topLeftDiagUp, .topLeftH]),
+        (40, [.topLeftDiagUp]),
+        (30, [.topLeftDiagDown]),
+        (20, [.topMiddleLeftH]),
+        (10, [.topLeftH]),
+        (9, [.topRightV, .topMiddleRightH, .topRightH]),
+        (8, [.topRightV, .topMiddleRightH]),
+        (7, [.topRightV, .topRightH]),
+        (6, [.topRightV]),
+        (5, [.topRightDiagUp, .topRightH]),
+        (4, [.topRightDiagUp]),
+        (3, [.topRightDiagDown]),
+        (2, [.topMiddleRightH]),
+        (1, [.topRightH])
+    ]
+    
+    var convertableValue = value
+    
+    var result = CistercianDrawing()
+    
+    for (number, lines) in data {
+        if number <= convertableValue {
+            result.insert(lines)
+            convertableValue = convertableValue - number
+        }
+    }
+    
+    return result
+}
+
+func convertValue2(value: Int) -> CistercianDrawing {
+    var conversionData: [(Int, CistercianDrawing)] = [
+        (9000, [.bottomLeftV, .bottomMiddleLeftH, .bottomLeftH]),
+        (8000, [.bottomLeftV, .bottomMiddleLeftH]),
+        (7000, [.bottomLeftV, .bottomLeftH]),
+        (6000, [.bottomLeftV]),
+        (5000, [.bottomMiddleLeftDiagDown, .bottomLeftH]),
+        (4000, [.bottomMiddleLeftDiagDown]),
+        (3000, [.bottomLeftDiagUp]),
+        (2000, [.bottomMiddleLeftH]),
+        (1000, [.bottomLeftH]),
+    ]
+
+    var convertMe = value
+    var result = CistercianDrawing()
+
+    for _ in 0...3 {
+        for (index, data) in conversionData.enumerated() {
+            if data.0 <= convertMe {
+                result.insert(data.1)
+                convertMe = convertMe - data.0
+            }
+            
+            conversionData[index] = (data.0 / 10, CistercianDrawing(rawValue: data.1.rawValue >> 5))
+        }
+    }
+    
+    return result
+}
